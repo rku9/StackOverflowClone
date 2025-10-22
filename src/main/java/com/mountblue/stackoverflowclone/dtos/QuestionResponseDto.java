9b4ae2835bffa091316baa2f31ff40b57f357715
nj8454 <nj8454@gmail.com>
2025-10-22 15:39:51 +0530

    created Question response Dto

--- src/main/java/com/mountblue/stackoverflowclone/dtos/QuestionResponseDto.java
+++ src/main/java/com/mountblue/stackoverflowclone/dtos/QuestionResponseDto.java
@@ -1,5 +1,17 @@
 package com.mountblue.stackoverflowclone.dtos;
 
-public class QuestionResponseDto {
+import java.time.LocalDateTime;
 
-}
+public record QuestionResponseDto(
+        Long questionId,
+        String authorName,
+        String title,
+        String body,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        int viewCount,
+        int score,
+        List<AnswerResponseDto> answers,
+        List<CommentResponseDto> comments,
+        List<TagResponseDto> tags
+) {}
