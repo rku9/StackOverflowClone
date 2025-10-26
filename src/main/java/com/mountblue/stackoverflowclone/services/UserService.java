65a0a6b7cbd4d113aba0f2b2b50dcc4f2857fc36
rushi-codes <rushikesh.kendre.34.2@mountblue.tech>
2025-10-26 11:35:03 +0530

    added register user method

--- src/main/java/com/mountblue/stackoverflowclone/services/UserService.java
+++ src/main/java/com/mountblue/stackoverflowclone/services/UserService.java
@@ -1,10 +1,32 @@
 package com.mountblue.stackoverflowclone.services;
 
+import com.mountblue.stackoverflowclone.models.User;
+import com.mountblue.stackoverflowclone.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {}
+
     public void register(String name, String email, String password, String confirmPassword) {
+    User user = new User();
+    user.setName(name);
+    user.setEmail(email);
+    if(passwordEncoder.matches(password, confirmPassword)) {
+        user.setPassword(passwordEncoder.encode(password));
+    }
+
+    userRepository.save(user);
     }
 }
