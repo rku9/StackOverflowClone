bdde1e9c32d54dbd4784205e3c7ba623ebd43838
nj8454 <nj8454@gmail.com>
2025-10-26 18:24:38 +0530

    created tags service

--- src/main/java/com/mountblue/stackoverflowclone/services/TagService.java
+++ src/main/java/com/mountblue/stackoverflowclone/services/TagService.java
@@ -1,8 +1,23 @@
 package com.mountblue.stackoverflowclone.services;
 
+import com.mountblue.stackoverflowclone.models.Tag;
+import com.mountblue.stackoverflowclone.repositories.TagRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class TagService {
 
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Tag> getAllTags() {
+        return tagRepository.findAllByOrderByNameAsc();
+    }
 }
