6d68e8f521c98b8d78044c68d4c50b32dc91c74b
rushi-codes <rushikesh.kendre.34.2@mountblue.tech>
2025-10-26 18:26:19 +0530

    tag controller created.

--- src/main/java/com/mountblue/stackoverflowclone/controllers/TagController.java
+++ src/main/java/com/mountblue/stackoverflowclone/controllers/TagController.java
@@ -1,10 +1,24 @@
 package com.mountblue.stackoverflowclone.controllers;
 
+import com.mountblue.stackoverflowclone.services.TagService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/tags")
 public class TagController {
 
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping
+    public String listTags(Model model) {
+        model.addAttribute("tags", tagService.getAllTags());
+        return "all-tags";
+    }
 }
