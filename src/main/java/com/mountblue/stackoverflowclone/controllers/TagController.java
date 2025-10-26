df38d0b34f8f12ad9bf705e7532001dd1dab0668
Rajat Upadhyay <rkucr7@gmail.com>
2025-10-26 16:37:29 +0530

    initiate tag feature branch

---
+++ src/main/java/com/mountblue/stackoverflowclone/controllers/TagController.java
@@ -0,0 +1,10 @@
+package com.mountblue.stackoverflowclone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tags")
+public class TagController {
+
+}
