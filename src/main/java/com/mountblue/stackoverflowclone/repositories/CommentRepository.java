1ee4edc11d7384b51a35d23d4e07a01b4a6ef7f2
Rajat Upadhyay <rkucr7@gmail.com>
2025-10-26 20:18:51 +0530

    initiate comment repo

--- src/main/java/com/mountblue/stackoverflowclone/repositories/CommentRepository.java
+++ src/main/java/com/mountblue/stackoverflowclone/repositories/CommentRepository.java
@@ -2,10 +2,31 @@ package com.mountblue.stackoverflowclone.repositories;
 
 import com.mountblue.stackoverflowclone.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Override
-    Optional<Comment> findById(Long commentId);
+
+    @Query("SELECT c FROM Comment c JOIN FETCH c.author WHERE c.question.id = :questionId ORDER BY c.createdAt ASC")
+    List<Comment> findByQuestionIdWithAuthor(@Param("questionId") Long questionId);
+
+    List<Comment> findByQuestionIdOrderByCreatedAtAsc(Long questionId);
+
+    List<Comment> findByAnswerIdOrderByCreatedAtAsc(Long answerId);
+
+    List<Comment> findByAuthorId(Long authorId);
+
+    Long countByQuestionId(Long questionId);
+
+    Long countByAnswerId(Long answerId);
+
+    void deleteByQuestionId(Long questionId);
+
+    void deleteByAnswerId(Long answerId);
 }
+
+
