0bd37c6193f7328b497ec65f1d12e6ee792f1f0d
nj8454 <nj8454@gmail.com>
2025-10-27 12:32:00 +0530

    created follow repository

--- src/main/java/com/mountblue/stackoverflowclone/repositories/FollowRepository.java
+++ src/main/java/com/mountblue/stackoverflowclone/repositories/FollowRepository.java
@@ -3,9 +3,16 @@ package com.mountblue.stackoverflowclone.repositories;
 import com.mountblue.stackoverflowclone.models.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     @Override
     Optional<Follow> findById(Long followId);
+
+    boolean existsByUser_IdAndObjectTypeAndObjectId(Long userId, String objectType, Long objectId);
+
+    Optional<Follow> findByUser_IdAndObjectTypeAndObjectId(Long userId, String objectType, Long objectId);
+
+    List<Follow> findAllByObjectTypeAndObjectId(String objectType, Long objectId);
 }
